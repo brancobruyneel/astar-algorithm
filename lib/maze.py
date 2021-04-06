c384b869e1db0ce8a03224d15265a81a7f71b33a
Branco <branco.bruyneel@gmail.com>
2021-04-06 13:34:27 +0200

    Add helper methods

--- lib/maze.py
+++ lib/maze.py
@@ -3,18 +3,19 @@ import pygame
 from queue import deque
 from random import choice
 
-from .cell import (
-    Cell,
-    Wall,
-    Start,
-    End
-)
+from .cell import (Cell, Wall, Start, End)
 
 
 class Maze:
     def __init__(self, size):
         self.width = size[0] // Cell.width
         self.height = size[1] // Cell.height
+        self.size = (self.width, self.height)
+        self.path = []
+
+        self.create_grid()
+
+    def create_grid(self):
         self.grid = [
             [
                 Wall(x, y, (self.width, self.height))
@@ -25,13 +26,33 @@ class Maze:
     def get(self, x, y):
         return self.grid[x][y]
 
-    def set(self, x, y, cell):
-        self.grid[x][y] = cell
+    def set(self, cell):
+        self.grid[cell.x][cell.y] = cell
+
+    def set_start(self, screen):
+        start = Start(self.path[0].x, self.path[0].y, self.size)
+        self.set(start)
+        start.draw(screen)
+        pygame.display.update()
+
+    def set_end(self, screen):
+        end = End(self.path[-1].x, self.path[-1].y, self.size)
+        self.set(end)
+        end.draw(screen)
+        pygame.display.update()
 
     def draw(self, screen):
         for row in self.grid:
             for cell in row:
                 cell.draw(screen)
+
+    def clean(self, screen):
+        self.create_grid()
+        self.draw(screen)
+
+    def color_unvisited(self, unvisited):
+        for cell in unvisited:
+            cell.surf.fill((255, 0, 0))
 
     def get_random_neighbor(self, current, unvisited):
         return choice(
@@ -43,26 +64,31 @@ class Maze:
         )
 
     def generate(self, screen=None, animate=False, sleep=10):
+        self.clean(screen)
+
         # Iterative implementation
         unvisited = [
             cell for row in self.grid for cell in row
             if cell.x % 2 and cell.y % 2
         ]
+
         current = unvisited.pop()
-        stack = deque()
+        stack = []
 
         while unvisited:
             try:
                 neighbor = self.get_random_neighbor(current, unvisited)
                 stack.append(current)
+
                 x = current.x - (current.x - neighbor.x) // 2
                 y = current.y - (current.y - neighbor.y) // 2
 
-                size = (self.width, self.height)
-                self.set(x, y, Cell(x, y, size))
-                self.set(
-                    current.x, current.y, Cell(current.x, current.y, size)
-                )
+                current_cell = Cell(current.x, current.y, self.size)
+                neighbor_cell = Cell(x, y, self.size)
+                self.set(current_cell)
+                self.set(neighbor_cell)
+                self.path.append(current_cell)
+                self.path.append(neighbor_cell)
 
                 current = neighbor
                 unvisited.remove(neighbor)
@@ -74,3 +100,10 @@ class Maze:
             except IndexError:
                 if stack:
                     current = stack.pop()
+
+        self.set_start(screen)
+        self.set_end(screen)
+
+
+    def search(self, screen=None, animate=False, sleep=10):
+        pass
--- main.py
+++ main.py
@@ -1,26 +1,48 @@
+import logging
 import pygame
 
-from pygame.locals import QUIT
+from pygame.locals import (
+    K_r,
+    K_SPACE,
+    K_ESCAPE,
+    KEYDOWN,
+    QUIT,
+)
 
 from lib.cell import Cell
 from lib.maze import Maze
 
+logging.basicConfig(level=logging.ERROR)
+
 pygame.init()
 
-WINSIZE = (Cell.width * 51, Cell.height * 51)
+WINSIZE = (Cell.width * 41, Cell.height * 41)
 
 screen = pygame.display.set_mode(WINSIZE)
 clock = pygame.time.Clock()
 
 maze = Maze(WINSIZE)
-maze.draw(screen)
-maze.generate(screen, True, 1)
 
+
+def draw_maze():
+    maze.draw(screen)
+    maze.generate(screen, True, 1)
+
+
+draw_maze()
 running = True
 
 while running:
     for event in pygame.event.get():
-        if event.type == QUIT:
+        if event.type == KEYDOWN:
+            if event.key == K_r:
+                draw_maze()
+            if event.key == K_SPACE:
+                # A* Search Algorithm
+                pass
+            elif event.key == K_ESCAPE:
+                running = False
+        elif event.type == QUIT:
             running = False
 
     pygame.display.flip()
